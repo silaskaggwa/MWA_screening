e9d0eadbf71d22b759021b738328ba84f8e3a980
Alem Embiale <alemwatch@gmail.com>
2018-10-22 18:35:39 -0500

    prefetch

--- routes/staff.js
+++ routes/staff.js
@@ -25,22 +25,21 @@ router.get('/invite', function(req, res, next) {
 });
 router.post('/invite', function(req, res, next) {
   console.log('gsfhdf>>', req.body)
-  // const data = {
-  //   status: config.invitation_status.SENT,
-  //   name: 'Silas',
-  //   email: 'silakag@gmail.com',
-  //   questions: ExamService.generateQuestions()
-  // };
-  // ExamService.createInvitation(data)
-  //   .then(invitation => {
-  //     AuthService.generate({id: invitation._id}).then(token => {
-  //       MailService.sendInvitationEmail(invitation.name, invitation.email, token);
-  //     });
-  //     res.status(200).json({success: true});
-  //   })
-  //   .catch(err => {
-  //     throw err
-  //   });
+  const data = {
+    status: config.invitation_status.SENT,
+    name: req.body.name,
+    email: req.body.email
+  };
+  ExamService.createInvitation(data)
+    .then(invitation => {
+      AuthService.generate({id: invitation._id}).then(token => {
+        MailService.sendInvitationEmail(invitation.name, invitation.email, token);
+      });
+      res.status(200).json({success: true});
+    })
+    .catch(err => {
+      throw err
+    });
 });
 
 module.exports = router;
